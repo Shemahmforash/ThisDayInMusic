b05e6cf1b1b703fd957398736987ef13e307d1f9
Carlos Rosão <shemahmforash@gmail.com>
2014-01-22 21:11:10 +0000

    Added a new Table Playlist that has a many to many relation with Track.

---
+++ src/Playlist.php
@@ -0,0 +1,54 @@
+<?php
+use Doctrine\Common\Collections\ArrayCollection;
+
+// src/Playlist.php
+/**
+ * @Entity @Table(name="Playlist",indexes={@index(name="date_idx", columns={"date"})})
+ */
+class Playlist {
+
+    /**
+     * @var int
+     */
+    /** @Id @Column(type="integer") @GeneratedValue **/
+    protected $id;
+
+    /**
+    * @ManyToMany(targetEntity="Track")
+    **/
+    protected $tracks;
+
+    /**
+     * @var int
+     */
+    /** @Column(type="date") **/
+    protected $date;
+
+    public function __construct() {
+        $this->tracks = new ArrayCollection();
+    }
+
+    public function addTrack( Track $track ) {
+        $this->tracks[] = $track;
+    }
+
+    /*Getters and setters*/
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getTracks() {
+        return $this->tracks;
+    }
+
+    public function getDate() {
+        return $this->date;
+    }
+
+    public function setDate(DateTime $date) {
+        $this->date = $date;
+    }
+
+}
+
+?>
