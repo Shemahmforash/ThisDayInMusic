af147bfb0c8fdce40e7cc5f0f1fd96173a068aff
Carlos Rosão <shemahmforash@gmail.com>
2014-02-17 11:56:08 +0000

    Moved the script that finds the events into a static method inside the Webservice and made the cron use it.

--- bin/events.php
+++ bin/events.php
@@ -3,139 +3,27 @@
 //cron each once a day at 00:00
 // 0 0 * * * /usr/bin/php pathtoproject/bin/artistTracks.php
 
-require_once __DIR__ . "/../vendor/autoload.php";
-require_once __DIR__ . "/../bootstrap.php";
+    require_once __DIR__ . "/../vendor/autoload.php";
+    require_once __DIR__ . "/../bootstrap.php";
 
-$str = file_get_contents( __DIR__ . "/date.txt");
+    $str = file_get_contents( __DIR__ . "/date.txt");
 
-#$date = new DateTime("now");
-$date = new \DateTime($str);
+    #$date = new DateTime("now");
+    $date = new \DateTime($str);
 
-if( $date->format('Y-m-d' ) >= "2014-04-01" ) {
-    error_log( "Date out of range: " . $date->format('Y-m-d') ); 
-    exit;
-}
+    #TODO: fix this comparison, it does not work like this
+    if( $date->format('Y-m-d' ) >= "2014-04-01" ) {
+        error_log( "Date out of range: " . $date->format('Y-m-d') ); 
+        exit;
+    }
 
-$query = $entityManager->createQuery('SELECT count(e) FROM Event e WHERE e.date LIKE :date');
-$query->setParameter("date", "%" . $date->format('m-d'));
-$eventNumber = $query->getSingleScalarResult();
+    $result = \Webservice\ThisDayInMusic::findEvents( $entityManager, $date );
+    if( $result == 0 ) {
+        error_log( "All events for " . $data->format('Y-m-d') . " imported sucessfully."); 
+    }
 
-if( $eventNumber ) {
-    error_log( "events for " . $date->format('Y-m-d') . " already exist. Exitting script." );
     date_add($date, date_interval_create_from_date_string('1 days'));
     $str = file_put_contents(__DIR__ . "/date.txt", $date->format("Y-m-d")  );
     exit;
-}
-
-error_log("date: " . $date->format('Y-m-d') );
-
-$dim = new \ThisDayIn\Music($date->format('j'), $date->format('F'));
-$evs = $dim->getEvents();
-
-foreach($evs as $ev ) {
-    $date   = new \DateTime( $ev['date'] );
-
-    if( $ev['type'] === 'Death') {
-        $ev['description'] = sprintf('%s, %s', $ev['name'], $ev['description']);
-    }
-
-    //unlike the death events, the birth events do not include enough information in the description.
-    if( $ev['type'] === 'Birth') {
-        $ev['description'] = sprintf('%s, %s was born', $ev['name'], $ev['description']);
-    }
-
-    //must find artist name for these kind of events
-    if( $ev['type'] == 'Event') {
-        $artist = findEventArtist( $ev['description'] );
-
-        $ev['name'] = $artist['name'];
-        if(isset( $artist['spotifyId'] ) )
-            $ev['spotifyId'] = $artist['spotifyId'];
-    }
-    #TODO: find artist spotify id for the other event types
-
-    //set current event
-    $event = new \Event(); 
-    $event->setDate( $date );
-    $event->setDescription( $ev['description'] ); 
-    $event->setType( $ev['type'] ); 
-    $event->setSource( $dim->getSource() ); 
-    $entityManager->persist( $event );
-
-    //connects the event to an artist
-    if( $ev['name'] ) {
-        $artist = $entityManager->getRepository('Artist')->findBy(array('name' => $ev['name']));
-
-        $artist = array_shift( $artist );
-
-        if(!$artist) {
-            $artist = new \Artist();
-            $artist->setName( $ev['name'] );
-
-            $event->setArtist( $artist );
-
-            if(isset( $ev['spotifyId'] ) )
-                $artist->setSpotifyId($ev['spotifyId']);
-        }
-
-        error_log("artist name: " . $artist->getName() );
-
-        $artist->assignToEvent( $event );
-
-        $entityManager->persist( $artist );
-
-        #one must save here so it copes for repeated artist in the event list
-        $entityManager->flush();
-    }
-}
-
-//insert all events to db
-if( count( $evs ) ) {
-    $entityManager->flush();
-}
-
-date_add($date, date_interval_create_from_date_string('1 days'));
-$str = file_put_contents(__DIR__ . "/date.txt", $date->format("Y-m-d")  );
-
-function findEventArtist( $text ) {
-
-    #get the configuration file for the app
-    $file = file_get_contents(__DIR__ . "/../etc/config.json");
-    $config = json_decode( $file, true );
-
-    \Echonest\Service\Echonest::configure($config['echonest']['key']);
-
-    $response = \Echonest\Service\Echonest::query('artist', 'extract', array(
-        'text'    => $text,
-        'sort'    => 'hotttnesss-desc',
-        'results' => '1',
-        'bucket'  => 'id:spotify-WW',
-    ));
-
-    if( $response && $response->response->status->code == 0 ) {
-
-        $artists =$response->response->artists;
-
-        if( !is_array(  $artists ) )
-            return;
-
-        if( !count( $artists ) )
-            return;
-
-        $artist = array_shift( $artists );
-        $return = array("name" => $artist->name );
-
-        error_log( "name = " . $artist->name );
-
-        if( isset( $artist->foreign_ids ) && is_array( $artist->foreign_ids ) ) {
-            $spotifyId = array_shift( $artist->foreign_ids );
-            $spotifyId = $spotifyId->foreign_id;
-
-            $return['spotifyId'] = $spotifyId;
-        }
-
-        return $return;
-    }
-}
 
 ?>
--- src/Webservice/ThisDayInMusic.php
+++ src/Webservice/ThisDayInMusic.php
@@ -191,6 +191,213 @@ abstract class ThisDayInMusic {
             $this->id = $parameters['id'];
         }
     }
+
+    #finds and sets the tracks for all trackless artists in the DB
+    public static function findArtistTracks( $entityManager ) {
+        #get the configuration file for the app
+        $file = file_get_contents(__DIR__ . "/../../etc/config.json");
+        $config = json_decode( $file, true );
+
+        $artistRepository = $entityManager->getRepository('Artist');
+        $artists = $artistRepository->findBy(array("hasTracks" => NULL));
+
+        if( count( $artists ) == 0  ) {
+            error_log( "no trackless artists." );
+            return;
+        }
+
+        foreach ( $artists as $artist ) {
+            $name = $artist->getName();
+
+            if(!$name)
+                continue;
+
+            #ignore artists that already have tracks
+            if($artist->getTracks()->count()) {
+                $entityManager->persist( $artist );
+                continue;
+            }
+
+            error_log( "processing $name" );
+
+            $spotifyId = $artist->getSpotifyId();
+
+            $parameters = array('bucket' => array('id:spotify-WW', 'songs'), 'limit'  => "true", "results" => 10);
+            if( $spotifyId )
+                $parameters['id'] = $spotifyId;
+            else
+                $parameters['artist'] = $name;
+            
+            $query = http_build_quer($parameters);
+            $query = preg_replace("/\%5B\d+\%5D/im", "", $query); 
+
+            \Echonest\Service\Echonest::configure($config['echonest']['key']);
+            $response = Echonest\Service\Echonest::query('song', 'search', $query);
+
+            $response = $response->response;
+
+            if( $response && $response->status->code == 0 ) {
+                $songs = $response->songs;
+
+                $entityManager->persist( $artist );
+
+                #no songs, move on to next artist
+                if(!count($songs)) {
+                    $artist->setHasTracks( false );
+                    continue;
+                }
+                else {
+                    $artist->setHasTracks( true );
+                }
+
+                foreach( $songs as $song ) {
+                    $track = new \Track();
+                    $title = $song->title;
+                    $track->setName( $title );
+
+                    $foreign = array_shift( $song->tracks );
+                    if( $foreign ) {
+                        $spotifyId = $foreign->foreign_id;
+
+                        $track->setSpotifyId( $spotifyId );
+                    }
+
+                    $artist->addTrack( $track );
+                    $track->assignToArtist( $artist );
+
+                    $entityManager->persist( $track );
+                    
+                }
+            }
+            else {
+                error_log("error: " . $response->status->message );
+                break;
+            }
+        }
+
+        $entityManager->flush();
+    }
+
+    #finds the events for a specific date a fills the database with them
+    public static function findEvents($entityManager, $date) {
+        error_log("date: " . $date->format('Y-m-d') );
+        $query = $entityManager->createQuery('SELECT count(e) FROM Event e WHERE e.date LIKE :date');
+        $query->setParameter("date", "%" . $date->format('m-d'));
+        $eventNumber = $query->getSingleScalarResult();
+
+        if( $eventNumber ) {
+            error_log( "events for " . $date->format('Y-m-d') . " already exist." );
+            return "1";
+        }
+
+        $dim = new \ThisDayIn\Music($date->format('j'), $date->format('F'));
+        $evs = $dim->getEvents();
+
+        foreach($evs as $ev ) {
+            $date   = new \DateTime( $ev['date'] );
+
+            if( $ev['type'] === 'Death') {
+                $ev['description'] = sprintf('%s, %s', $ev['name'], $ev['description']);
+            }
+
+            //unlike the death events, the birth events do not include enough information in the description.
+            if( $ev['type'] === 'Birth') {
+                $ev['description'] = sprintf('%s, %s was born', $ev['name'], $ev['description']);
+            }
+
+            //must find artist name for these kind of events
+            if( $ev['type'] == 'Event') {
+                $artist = self::findEventArtist( $ev['description'] );
+
+                $ev['name'] = $artist['name'];
+                if(isset( $artist['spotifyId'] ) )
+                    $ev['spotifyId'] = $artist['spotifyId'];
+            }
+            #TODO: find artist spotify id for the other event types
+
+            //set current event
+            $event = new \Event(); 
+            $event->setDate( $date );
+            $event->setDescription( $ev['description'] ); 
+            $event->setType( $ev['type'] ); 
+            $event->setSource( $dim->getSource() ); 
+            $entityManager->persist( $event );
+
+            //connects the event to an artist
+            if( $ev['name'] ) {
+                $artist = $entityManager->getRepository('Artist')->findBy(array('name' => $ev['name']));
+
+                $artist = array_shift( $artist );
+
+                if(!$artist) {
+                    $artist = new \Artist();
+                    $artist->setName( $ev['name'] );
+
+                    $event->setArtist( $artist );
+
+                    if(isset( $ev['spotifyId'] ) )
+                        $artist->setSpotifyId($ev['spotifyId']);
+                }
+
+                error_log("artist name: " . $artist->getName() );
+
+                $artist->assignToEvent( $event );
+
+                $entityManager->persist( $artist );
+
+                #one must save here so it copes for repeated artist in the event list
+                $entityManager->flush();
+            }
+        }
+
+        //insert all events to db
+        if( count( $evs ) ) {
+            $entityManager->flush();
+        }
+        
+        return 0;
+    }
+
+    public static function findEventArtist( $text ) {
+
+        #get the configuration file for the app
+        $file = file_get_contents(__DIR__ . "/../../etc/config.json");
+        $config = json_decode( $file, true );
+
+        \Echonest\Service\Echonest::configure($config['echonest']['key']);
+
+        $response = \Echonest\Service\Echonest::query('artist', 'extract', array(
+            'text'    => $text,
+            'sort'    => 'hotttnesss-desc',
+            'results' => '1',
+            'bucket'  => 'id:spotify-WW',
+        ));
+
+        if( $response && $response->response->status->code == 0 ) {
+
+            $artists =$response->response->artists;
+
+            if( !is_array(  $artists ) )
+                return;
+
+            if( !count( $artists ) )
+                return;
+
+            $artist = array_shift( $artists );
+            $return = array("name" => $artist->name );
+
+            error_log( "name = " . $artist->name );
+
+            if( isset( $artist->foreign_ids ) && is_array( $artist->foreign_ids ) ) {
+                $spotifyId = array_shift( $artist->foreign_ids );
+                $spotifyId = $spotifyId->foreign_id;
+
+                $return['spotifyId'] = $spotifyId;
+            }
+
+            return $return;
+        }
+    }
 }
 
 ?>
