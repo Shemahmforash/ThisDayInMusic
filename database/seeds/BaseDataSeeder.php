89143904aa187a8c32d2a11315b42a31f968e6f3
Carlos Rosão <shemahmforash@hotmail.com>
2015-07-29 20:52:02 +0000

    Improved data generation factories

--- database/seeds/BaseDataSeeder.php
+++ database/seeds/BaseDataSeeder.php
@@ -11,12 +11,21 @@ class BaseDataSeeder extends Seeder
      */
     public function run()
     {
-        // factory(\ThisDayInMusic\Artist::class, 30)->create();
+        //factory(\ThisDayInMusic\Artist::class, 30)->create();
 
         // factory(\ThisDayInMusic\Track::class, 60)->create()->each(function ($track) {
-        //     $track->artist()->associate(factory(\ThisDayInMusic\Artist::class)->create());
+        //       $track->artist()->save(factory(\ThisDayInMusic\Artist::class)->create());
         // });
 
-        factory(\ThisDayInMusic\User::class, 1)->create();
+        // factory(\ThisDayInMusic\Event::class, 60)->create()->each(function ($event) {
+        //       $event->artist()->associate(factory(\ThisDayInMusic\Artist::class)->create());
+        // });
+
+        factory(\ThisDayInMusic\Artist::class, 60)->create()->each(function ($artist) {
+              $artist->tracks()->save(factory(\ThisDayInMusic\Track::class)->create());
+              $artist->events()->save(factory(\ThisDayInMusic\Event::class)->create());
+        });
+
+        // factory(\ThisDayInMusic\User::class, 1)->create();
     }
 }
