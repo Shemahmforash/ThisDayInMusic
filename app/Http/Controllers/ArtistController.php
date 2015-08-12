42bc198600e5aacb7bf3ee56a2aaf9f0486bbdad
Carlos RosÃ£o <shemahmforash@hotmail.com>
2015-08-12 17:08:13 +0100

    Removed more unnecessary methods from controllers

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -35,49 +35,4 @@ class ArtistController extends Controller
         return $this->response->item($artist, new \ThisDayInMusic\Http\Transformers\ArtistTransformer);
 
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int      $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  Request  $request
-     * @param  int      $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int      $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
